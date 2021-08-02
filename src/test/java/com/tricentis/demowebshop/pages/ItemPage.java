4b966ec3af893cf6086f5c56ecc4d35c668755d1
Egor Muratov <egormuratov@yahoo.com>
2021-08-02 22:29:26 +0300

    created UI test "addItemByUI"

---
+++ src/test/java/com/tricentis/demowebshop/pages/ItemPage.java
@@ -0,0 +1,29 @@
+package com.tricentis.demowebshop.pages;
+
+import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+
+public class ItemPage {
+    SelenideElement itemTitleH1 = $("[itemprop='name']"),
+            countItemInShoppingCart = $(".cart-qty"),
+            btnAddCart = $(".add-to-cart-button");
+
+    @Step("Проверить что название товара: {$itemName}")
+    public void checkTitleItem(String itemName) {
+        itemTitleH1.shouldHave(text(itemName));
+    }
+
+    @Step("Получить кол-во товаров в корзине")
+    public void checkCountItemsInShoppingCart(int count) {
+        countItemInShoppingCart.shouldHave(text(String.format("(%s)", count)));
+    }
+
+    @Step("Нажать на кнопку добавления товара в корзину")
+    public void addItemInShoppingCart() {
+        btnAddCart.click();
+    }
+
+}
--- src/test/java/com/tricentis/demowebshop/tests/ShoppingCartTests.java
+++ src/test/java/com/tricentis/demowebshop/tests/ShoppingCartTests.java
@@ -1,5 +1,24 @@
 package com.tricentis.demowebshop.tests;
 
-public class ShoppingCartTests extends TestBase {
+import com.tricentis.demowebshop.pages.ItemPage;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Selenide.open;
+
+public class ShoppingCartTests extends TestBase {
+    ItemPage itemPage = new ItemPage();
+
+    @BeforeEach
+    void openSite() {
+        open("/build-your-cheap-own-computer");
+    }
+
+    @Test
+    void addItemByUI() {
+        itemPage.checkTitleItem("Build your own cheap computer");
+        itemPage.checkCountItemsInShoppingCart(0);
+        itemPage.addItemInShoppingCart();
+        itemPage.checkCountItemsInShoppingCart(1);
+    }
 }
